4a7a8b634a1567fcd59e51b44485edde827fdb65
Supatsara Wattanakriengkrai <panda_asia12@hotmail.com>
2019-04-17 21:12:26 +0700

    Add files via upload

--- comment.js
+++ comment.js
@@ -1,23 +1,3 @@
-
-
-
-const COMMENT_START = /(^([ \t]+)?\/\*|^([ \t]+)?###)/gi;
-
-const COMMENT_END = /(\*\/|^([ \t]+)?###)/gi;
-
-const COMMENT_SYNTAX = /(^[ \t]+\/\* ?|^[ \t]+#(##)? ?| ?\*\/|^[ \t]+|[ \t]+$)/gi;
-
-const COMMENT_RE = /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm;
-
-var keepcomment = [];
-
-function print(xhrtext){
-  var receive = JSON.parse(xhrtext);
-  console.log(receive);
-
-}
-
-// Check it out; also ugly
 class Line {
 
     /**
@@ -33,113 +13,7 @@ class Line {
       this.parentNode = node.parentNode;
     }
   
-// TODO: Add empty placeholder here?
-  
-    /**
-     * Render line (adds comment style).
-     * @public
-     * @instance
-     * @method render
-     */
-    render() {
-      this.parentNode.classList.add('blob-expanded');
-    }
-  
-    /**
-     * Show line.
-     * @public
-     * @instance
-     * @method show
-     */
-    show() {
-      this.parentNode.style.display = '';
-    }
-  
-    /**
-     * Hide line.
-     * @public
-     * @instance
-     * @method hide
-     */
-    hide() {
-      this.parentNode.style.display = 'none';
-    }
-  
-    /**
-     * Cleanup and destroy line.
-     * @public
-     * @instance
-     * @method destroy
-     */
-    destroy() {
-      delete this.node;
-      delete this.index;
-      delete this.text;
-      delete this.parentNode;
-    }
   }
-
-class Comment {
-
-    /**
-     * @public
-     * @instance
-     * @method constructor
-     * @param {Line[]} lines - The lines of the comment.
-     */
-    constructor(lines) {
-      this.lines = lines;
-      this.length = this.lines.length;
-      this.start = this.lines[0];
-      this.end = this.lines[this.length - 1];
-    }
-  
-    /**
-     * Render the comment (creating a toggle).
-     * @public
-     * @instance
-     * @method render
-     */
-    render() {
-      this.lines.forEach(line => line.render());
-    }
-  
-    /**
-     * Show the comment.
-     * @public
-     * @instance
-     * @method show
-     */
-    show() {
-      this.lines.forEach(line => line.show());
-  
-    }
-  
-    /**
-     * Hide the comment.
-     * @public
-     * @instance
-     * @method hide
-     */
-    hide() {
-      this.lines.forEach(line => line.hide());
-  
-    }
-  
-    /**
-     * Cleanup and destroy comment.
-     * @public
-     * @instance
-     * @method destroy
-     */
-    destroy() {
-      delete this.lines;
-      delete this.start;
-      delete this.end;
-      delete this.toggle;
-    }
-  }
-  
   /**
    * @public
    * @class File
@@ -153,13 +27,9 @@ class Comment {
      */
     constructor() {
       this.nodes = document.getElementsByClassName('blob-code');
-      this.lines = [];
       this.comments = [];
       this.length = 0;
       this.parse();
-      this.render();
-      //this.show2();
-      this.send();
     }
   
     /**
@@ -169,54 +39,18 @@ class Comment {
      * @method parse
      */
     parse() {
-      var comment = false;
-      var lines;
+
       this.length = this.nodes.length;
       
   
       for (var index = 0, length = this.nodes.length; index < length; index++) {
         var node = this.nodes[index];
         var line = new Line(node, index);
-        keepcomment.push(line.text + " newnnnn ");
-  
-        this.lines.push(line);
-  
-        if (!comment && COMMENT_START.test(line.text)) {
-          lines = [];
-          comment = true;
-        }
-  
-        if (comment) {
-          lines.push(line);
-        }
-  
-        if (comment && COMMENT_END.test(line.text)) {
-          this.comments.push(new Comment(lines));
-          comment = false;
-        }
+        this.comments.push(line.text + " newlinecom ");
+
       }
     }
   
-    /**
-     * Render the files comments.
-     * @public
-     * @instance
-     * @method show
-     */
-    render() {
-      this.comments.forEach(comment => comment.render());
-    }
-  
-    /**
-     * Show file comments.
-     * @public
-     * @instance
-     * @method hide
-     */
-    show() {
-      this.comments.forEach(comment => comment.show());
-      
-    }
 
     show2()
     {
@@ -228,28 +62,11 @@ class Comment {
             });
         }
     }
-    send()
-    {
-     
-      }
+    
 
-    
-    
-  
-    /**
-     * Hide file comments.
-     * @public
-     * @instance
-     * @method hide
-     */
-    hide() {
-      var lineNum = +location.hash.replace('#L', '');
-  
-      this.comments.forEach(comment => {
-        if (lineNum <= comment.start.index || lineNum >= comment.end.index) {
-          comment.hide();
-        }
-      });
+    return_comment()
+    {
+      return this.comments;
     }
   
     /**
@@ -259,11 +76,11 @@ class Comment {
      * @method destroy
      */
     destroy() {
-      this.comments.forEach(comment => comment.destroy());
-      this.lines.forEach(line => line.destroy());
+      //this.comments.forEach(comment => comment.destroy());
+      //this.lines.forEach(line => line.destroy());
       delete this.nodes;
-      delete this.lines;
-      delete this.comments;
+      //delete this.lines;
+      //delete this.comments;
     }
   }
 
@@ -276,8 +93,10 @@ class Extension {
      */
     constructor() {
       this.container = document.getElementById('js-repo-pjax-container');
+      this.comments = [];
       this.render();
       this.observe();
+      
     }
   
     /**
@@ -307,6 +126,8 @@ class Extension {
      */
     render() {
       this.file = new File();
+      this.comments = this.file.return_comment();
+      
      
   
     }
@@ -324,59 +145,256 @@ class Extension {
  
     }
   }
-  
-  /**
-   * Kick off extension.
-   * @type {Extension}
-   */
-  
-  (() => new Extension())();
 
-  // TODO: allow user to input a source code folder
-
-  const API_URL = 'http://127.0.0.1:5000/';
-  const input = JSON.stringify(keepcomment);
-
-  const xhr = new XMLHttpRequest();
-  const data = new FormData();
-  
-  data.append('input', input);
-
-  xhr.open('POST', API_URL, true);
-
-  xhr.onreadystatechange = function () {
-    if(xhr.readyState == 4) {
-      console.log("Hi")
-      console.log(xhr.status)
-      if(xhr.status == 200 || xhr.status == 0)
-      {
-      console.log(xhr.responseText);
-      var receiveJson = JSON.parse(xhr.responseText);
-      console.log(receiveJson['comment'])
-      
-      var i;
-      for (i = 1; i < 2; i++) { 
-        text = "LC"+ i.toString();
-        document.getElementById(text).style.backgroundColor = "orange";
-        document.getElementById(text).style.color = "black";
-      }
-     
-     
-      }
-   
-
-     
-    }
-    else{
-      console.log("Hello");
-    }
+  function highlightDesign(comments) 
+  {
     
-  };
+    var count = Object.keys(comments).length;
+    console.log(count);
 
-  xhr.send(data);
+    for (i = 0; i < count; i++) 
+    { 
+      var num = i.toString();
+      console.log(comments[num]["comment"]);
+      console.log(comments[num]["linestart"]);
+      console.log(comments[num]["lineend"]);
 
+      var line_start = comments[num]["linestart"];
+      var line_end = comments[num]["lineend"];
+      var label = comments[num]["label"];
+      
+
+      for (j = line_start; j < line_end + 1; j++) 
+      {
+        if(label == "design")
+        {
+          text = "LC"+ j.toString();
+          document.getElementById(text).style.backgroundColor = "#FA8072";
+          document.getElementById(text).style.color = "black";
+        }
+         
+        
+      }
+
+    }
+  
+  }
+
+  function unhighlightDesign(comments) {
+    
+    var count = Object.keys(comments).length;
+    console.log(count);
+
+    for (i = 0; i < count; i++) 
+    { 
+      var num = i.toString();
+      console.log(comments[num]["comment"]);
+      console.log(comments[num]["linestart"]);
+      console.log(comments[num]["lineend"]);
+
+      var line_start = comments[num]["linestart"];
+      var line_end = comments[num]["lineend"];
+      var label = comments[num]["label"];
+      
+
+      for (j = line_start; j < line_end + 1; j++) 
+      {
+        if(label == "design")
+        {
+          text = "LC"+ j.toString();
+          document.getElementById(text).style.backgroundColor = "";
+          document.getElementById(text).style.color = "";
+        }
+         
+        
+      }
+
+    }
+  
+  }
+
+  function highlightReq(comments) 
+  {
+    
+    var count = Object.keys(comments).length;
+    console.log(count);
+
+    for (i = 0; i < count; i++) 
+    { 
+      var num = i.toString();
+      console.log(comments[num]["comment"]);
+      console.log(comments[num]["linestart"]);
+      console.log(comments[num]["lineend"]);
+
+      var line_start = comments[num]["linestart"];
+      var line_end = comments[num]["lineend"];
+      var label = comments[num]["label"];
+      
+
+      for (j = line_start; j < line_end + 1; j++) 
+      {
+
+        if(label == "req")
+        {
+          text = "LC"+ j.toString();
+          document.getElementById(text).style.backgroundColor = "#FFD700";
+          document.getElementById(text).style.color = "black";
+        }
+         
+        
+      }
+
+    }
+  
+  }
+
+  function unhighlightReq(comments) 
+  {
+    
+    var count = Object.keys(comments).length;
+    console.log(count);
+
+    for (i = 0; i < count; i++) { 
+      var num = i.toString();
+      console.log(comments[num]["comment"]);
+      console.log(comments[num]["linestart"]);
+      console.log(comments[num]["lineend"]);
+
+      var line_start = comments[num]["linestart"];
+      var line_end = comments[num]["lineend"];
+      var label = comments[num]["label"];
+      
+
+      for (j = line_start; j < line_end + 1; j++) {
+
+        if(label == "req")
+        {
+          text = "LC"+ j.toString();
+          document.getElementById(text).style.backgroundColor = "";
+          document.getElementById(text).style.color = "";
+        }
+         
+        
+      }
+
+    }
+  
+  }
+
+  function sendcommentstoServer(comment, command)
+  {
+    const API_URL = 'http://127.0.0.1:5000/';
+    const input = JSON.stringify(comment);
+    const xhr = new XMLHttpRequest();
+    const data = new FormData();
+    
+    data.append('input', input);
+  
+    xhr.open('POST', API_URL, true);
+  
+    xhr.onreadystatechange = function ()
+     {
+      if(xhr.readyState == 4) 
+      {
+        console.log(xhr.status)
+        if(xhr.status == 200 || xhr.status == 0)
+        {
+          var receiveJson = JSON.parse(xhr.responseText);
+          console.log(receiveJson);
+          chrome.storage.sync.set({'comments': receiveJson}, function() {
+          
+          });
+            
+          if(command == "DetectGitHub"){
+
+            chrome.storage.sync.set({'finishcomments': "On"}, function() {
+            
+          
+            });
+          }
+
+          if(command == "DetectFile")
+          {
+
+            chrome.storage.sync.set({'showReport': "On"}, function() {
+            });
+          }
+       
+       
+        }
+       
+      }
+    };
+  
+    xhr.send(data);
+  
+  }
+  //activate by the front-end
+  chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) 
+    {
+
+      var storageChange = changes[key];
+
+      if(key == "Open" && storageChange.newValue == "On") 
+      {
+        var ex = new Extension();
+        sendcommentstoServer(ex.comments, "DetectGitHub");
+      }
+
+      if(key == "Open" && storageChange.newValue == "Off") 
+      {
+        chrome.storage.sync.get(['comments'], function(result) {
+          unhighlightDesign(result.comments);
+          unhighlightReq(result.comments);
+        });
+      }
+
+      if(key == "markDesign" && storageChange.newValue == "On")
+      {
+          chrome.storage.sync.get(['comments'], function(result) {
+            highlightDesign(result.comments);
+          });
+      }
+
+      if(key == "markDesign" && storageChange.newValue == "Off")
+      {
+          chrome.storage.sync.get(['comments'], function(result) {
+            unhighlightDesign(result.comments);
+          });
+      
+      }
+
+      if(key == "markReq" && storageChange.newValue == "On")
+      {
+          chrome.storage.sync.get(['comments'], function(result) {
+            highlightReq(result.comments);
+          });
+      }
+
+      if(key == "markReq" && storageChange.newValue == "Off")
+      {
+          chrome.storage.sync.get(['comments'], function(result) {
+            unhighlightReq(result.comments);
+          });
+      
+      }
+
+      if(key == "file" && storageChange.newValue == "On")
+      {
+        chrome.storage.local.get(['codes'], function(result) {
+        var str1 = JSON.parse(JSON.stringify(result));
+        sendcommentstoServer(str1['codes'],"DetectFile");
+        });
+      }
+    }
+  });
+
+  
 
  
+	 
+
  
   
   
